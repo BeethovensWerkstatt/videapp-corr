672c9b8371b0c346353afbc819a20e55e2e1151d
Jan-Peter Voigt <jpv@schoene-noten.de>
2021-01-19 14:52:17 +0100

    ZoneComponent style

--- src/components/ZoneComponent.vue
+++ src/components/ZoneComponent.vue
@@ -1,9 +1,17 @@
 <template>
-    <div :id="divid" class="zone-component">
+    <div :id="divid" class="zone-component" :style="style">
     </div>
 </template>
 
 <script>
+/**
+ * @module components/ZoneComponent
+ * @vue-prop {number} x - horizontal coordinate on page
+ * @vue-prop {number} y - vertical coordinate on page
+ * @vue-prop {number} width - width of measure zone
+ * @vue-prop {number} height - height of measure zone
+ * @vue-computed {object} style - calculated style
+ */
 export default {
   name: 'ZoneComponent',
   props: {
@@ -22,6 +30,16 @@ export default {
     height: {
       type: Number,
       required: true
+    }
+  },
+  computed: {
+    style () {
+      return {
+        x: this.x + 'px',
+        y: this.y + 'px',
+        width: this.width + 'px',
+        height: this.height + 'px'
+      }
     }
   }
 }
