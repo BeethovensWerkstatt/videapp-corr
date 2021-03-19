9aa0e891f1d963c4c2a33d37c64a01262cacd97b
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2021-03-19 16:44:15 +0100

    init gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const git = require('gulp-git')
 
 gulp.task('gitlog', async function () {
-  git.exec({ args: 'log -n 1 --pretty="%ai %H%n%s (%an)"' }, function (err, stdout) {
+  git.exec({ args: 'log -n 1 --pretty="%ai %H (%an)%n%s"', log: true }, function (err, stdout) {
     if (err) throw err
   })
 })
