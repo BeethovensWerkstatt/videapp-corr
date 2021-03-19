c23d59739694efc77b2fafaff4d4a34a6d1acbe7
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2021-03-19 21:19:18 +0100

    gulp version.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 const gulp = require('gulp')
 const git = require('gulp-git')
+const fs = require('fs')
 
 gulp.task('gitlog', async function () {
-  git.exec({ args: 'log -n 1 --pretty="%ai %H (%an)%n%s"', log: true }, function (err, stdout) {
-    if (err) throw err
-  })
+  git.exec(
+    {
+      args: 'log -n 1 --pretty="%ai %H (%an)%n%s"',
+      log: false
+    },
+    function (err, stdout) {
+      if (err) throw err
+      // store in config/version.json
+      const json = { version: stdout }
+      fs.writeFile('src/config/version.json', JSON.stringify(json, null, 2), function () {
+        console.log('fertig')
+      })
+    }
+  )
 })
