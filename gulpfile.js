564594f1ceff6b9260098c75f6ef305162f59f36
Jan-Peter Voigt <jpv@schoene-noten.de>
2021-04-12 12:08:07 +0200

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 gulp.task('gitlog', async function () {
   git.exec(
     {
-      args: 'log -n 1 --pretty="%ai%n(%an)%n%s"%H%n',
+      args: 'log -n 1 --pretty="%ai%n(%an)%n%s%n%H%n"',
       log: false
     },
     function (err, stdout) {
