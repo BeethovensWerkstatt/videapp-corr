38ca4b8354b56666b4028d21817cac3d39ae20ef
Jan-Peter Voigt <jpv@schoene-noten.de>
2021-07-01 16:36:13 +0200

    refactor complaint tab: zoom facsimiles

--- src/components/ComplaintDialog/TabColFacs.vue
+++ src/components/ComplaintDialog/TabColFacs.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="facsView">
     <h2>{{ label }}</h2>
-    <img :src="src" :style="{ width: '100%' }" />
+    <input type="range" min="25" max="150" class="slider" v-model="izoom" />
+    <img :src="src" :style="{ width: izoom + '%' }" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'ComplaintDialogTabColFacs',
+  data () {
+    return {
+      izoom: 100
+    }
+  },
   props: {
     src: {
       type: String,
@@ -23,7 +29,11 @@ export default {
 
 <style lang="scss" scoped>
 .facsView {
+  width: 100%;
   overflow: auto;
   resize: vertical;
+}
+.slider {
+  width: 90%;
 }
 </style>
