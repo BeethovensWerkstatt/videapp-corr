0111a54e3ffe5c5bc5a8dac3d3ab14481fa0fa34
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2021-09-22 13:07:16 +0200

    TabColVerovio refactor to OSD [WIP]

--- src/components/ComplaintDialog/TabColVerovio.vue
+++ src/components/ComplaintDialog/TabColVerovio.vue
@@ -11,18 +11,20 @@
     </div>
     -->
     <div :id="divid" class="ComplaintDialodVOSD">
-      <div :id="vid" />
+      <div :id="vid" v-html="svg" />
     </div>
   </div>
 </template>
 
 <script>
 // import Vue from 'vue'
-// import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 // import VerovioComponent from '@/components/VerovioComponent.vue'
 import OpenSeadragon from 'openseadragon'
+import axios from 'axios'
 import config from '@/config'
 import { desktopTile } from '@/toolbox'
+import { startProc, finishProc } from '@/store'
 
 /**
  * @module components/ComplaintDialog/TabColVerovio
@@ -36,7 +38,10 @@ export default {
   data () {
     return {
       viewer: undefined,
-      viewerprops: { ...config.osd, ...this.osdinit }
+      viewerprops: { ...config.osd, ...this.osdinit },
+      toolkit: undefined,
+      mei: undefined,
+      svg: undefined
     }
   },
   props: {
@@ -75,6 +80,12 @@ export default {
     this.viewer.addTiledImage({
       tileSource: TIback
     })
+    this.viewer.addOverlay({
+      element: this.$el.querySelector('#' + this.vid),
+      location: OpenSeadragon.Point(0, 0)
+    },
+    OpenSeadragon.Point(0, 0))
+    this.loadMEI()
   },
   beforeDestroy () {
     if (this.viewer) {
@@ -82,6 +93,7 @@ export default {
     }
   },
   computed: {
+    ...mapGetters(['vrvToolkit']),
     divid () {
       return this.vid + '_ovl'
     },
@@ -106,6 +118,83 @@ export default {
       }
       // console.log(props)
       return props
+    },
+    vrvOptions () {
+      const opts = {}
+      // TODO config?
+      opts.scale = 30
+      opts.pageWidth = 10000
+      opts.outputIndent = 1
+      opts.adjustPageWidth = true
+      opts.adjustPageHeight = true
+      opts.svgBoundingBoxes = true
+      opts.svgViewBox = true
+      return opts
+    }
+  },
+  methods: {
+    loadMEI () {
+      // console.log(this.options)
+      if (this.options?.url) {
+        startProc()
+        console.log('load', this.options.url)
+        this.mei = undefined
+        this.svg = undefined
+        axios.get(this.options.url).then(({ data }) => {
+          this.mei = data
+          if (this.mei) {
+            // console.log(this.mei)
+            if (!this.toolkit) {
+              this.toolkit = this.vrvToolkit()
+            }
+            this.toolkit.setOptions(this.vrvOptions)
+            this.toolkit.loadData(this.mei)
+            this.svg = this.toolkit.renderToSVG(1)
+            const parser = new DOMParser()
+            const dsvg = parser.parseFromString(this.svg, 'image/svg+xml')
+            const viewBox = dsvg.activeElement.viewBox.baseVal
+            console.log(viewBox, viewBox.height / viewBox.width)
+          }
+        }).catch(error => {
+          console.error(error)
+        }).finally(() => {
+          finishProc()
+        })
+      }
+    },
+    /**
+     * taken from module 1 VideApp
+     */
+    _getVerovioDimensions (renderedSvg) {
+      try {
+        const viewBoxHeight = parseInt(renderedSvg.querySelector('svg.definition-scale').getAttribute('viewBox').split(' ')[3], 10)
+        const firstStaffLineYPos = parseInt(renderedSvg.querySelector('g.measure g.staff path:first-of-type').getAttribute('d').split(' ')[1], 10)
+        const lastStaffLineYPos = parseInt(renderedSvg.querySelector('g.measure g.staff path:last-of-type').getAttribute('d').split(' ')[1], 10)
+
+        const staffHeight = lastStaffLineYPos - firstStaffLineYPos
+        const staffHeightRelation = staffHeight / viewBoxHeight
+
+        const widthAttr = renderedSvg.childNodes[0].getAttribute('width')
+        const width = parseInt(widthAttr, 10)
+
+        const heightAttr = renderedSvg.childNodes[0].getAttribute('height')
+        const height = parseInt(heightAttr, 10)
+
+        return {
+          relation: staffHeightRelation,
+          staffHeight: staffHeight,
+          viewBoxHeight: viewBoxHeight,
+          width: width,
+          height: height
+        }
+      } catch (err) {
+        console.log('')
+        console.log('[ERROR] Unable to determine dimensions of rendered SVG: ' + err)
+        console.log(renderedSvg)
+        return {
+          relation: -1
+        }
+      }
     }
   }
 }
