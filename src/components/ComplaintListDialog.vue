709f517180f278d66021b096909b0737a4ed07f7
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2021-11-12 15:02:48 +0100

    ComplaintListDialog skeleton

---
+++ src/components/ComplaintListDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="dialogBack" :class="{ 'inactive': !this.active }">
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ComplaintListDialog',
+  props: {},
+  data: () => ({}),
+  mounted () {
+    window.addEventListener('resize', this.resize)
+  },
+  watch: {
+    active () {
+      if (this.active) {
+        this.viewer.navigator.element.style.display = 'none'
+      } else {
+        this.viewer.navigator.element.style.display = 'inline-block'
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['viewer']),
+    active () {
+      return false
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+.dialogBack {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 10;
+  background-color: rgba(0,0,0,.3);
+  backdrop-filter: blur(3px);
+  -webkit-backdrop-filter: blur(3px);
+}
+
+.inactive {
+  display: none;
+}
+
+.dialog {
+  position: absolute;
+  left: 1rem;
+  top: 1rem;
+  // TODO ??
+  width: calc(100% - 2rem);
+  height: calc(100% - 2rem);
+  border-radius: 5px;
+  background-color: white;
+  border: .5px solid #666666;
+  box-shadow: 0 0 .8rem #00000066;
+  border-radius: .3rem;
+}
+
+</style>
