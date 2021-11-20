c3e3670d8561ed229aed90ade3d55779c4460eb5
Johannes Kepper <kepper@edirom.de>
2021-11-20 13:17:20 +0100

    adjust perspective selection layout

--- src/components/SelectComplaintViews.vue
+++ src/components/SelectComplaintViews.vue
@@ -2,11 +2,11 @@
   <div class="dialogBack" :class="{ 'inactive': !this.select.dialog }">
     <div class="dialog">
       <div class="head">
+        <h3>Ansichtseinstellungen</h3><!-- TODO: Get this into i18n -->
         <div class="close">
           <button class="btn btn-sm" @click.prevent="closeDialog"><i class="icon icon-cross"></i> {{ $t('terms.close') }}</button>
         </div>
       </div>
-
       <div class="selectTab">
         <div class="titleRow">
           <div />
@@ -17,7 +17,7 @@
             @click="toggle(state)"
             :class="{ selected: select[state] }"
           >
-            {{ state }}
+            {{ columnTitle[state] }}
           </div>
         </div>
         <div
@@ -28,16 +28,16 @@
           <div
             class="selector"
             @click="toggle(artefact)"
-            :class="{ selected: select[artefact] }"
+            :class="[{ selected: select[artefact] }, artefact]"
           >
-            {{ artefact }}
+            {{ rowTitle[artefact] }}
           </div>
           <div
             v-for="state in ['ante', 'rvsn', 'post']"
             :key="state"
-            :class="{ selected: select[state] && select[artefact] }"
+            :class="[{ selected: select[state] &&  select[artefact] }, artefact, state]"
           >
-            <span v-if="select[state] && select[artefact]">&times;</span>
+            <span v-if="select[state] && select[artefact]">✓</span>
           </div>
         </div>
       </div>
@@ -60,6 +60,12 @@ export default {
       set (sel) {
         this.$store.commit('SET_COMPLAINT_DISPLAY_SELECT', sel)
       }
+    },
+    columnTitle () {
+      return { ante: 'ante revisionem', rvsn: 'revisio', post: 'post revisionem' }
+    },
+    rowTitle () {
+      return { facs: 'Dokument', trns: 'Annotierte Transkription', text: 'Ausgangs- / Zieltext', anno: 'Kommentar' }
     }
   },
   methods: {
@@ -99,6 +105,8 @@ export default {
   height: 300px; // auto by content
   background-color: white;
   border-radius: 5px;
+  border: .5px solid #333333;
+  box-shadow: 0 .5rem 2rem #00000066;
 
   .head {
     height: 3rem;
@@ -107,6 +115,12 @@ export default {
     text-align: left;
     background: linear-gradient(180deg, #cccccc 0%, #f5f5f5 100%);
     border-radius: .3rem .3rem 0 0;
+
+    h3 {
+      position: relative;
+      top: .8rem;
+      font-size: 1.2rem;
+    }
 
     .close {
       position: absolute;
@@ -117,7 +131,8 @@ export default {
 
   .selectTab {
     display: table;
-    margin: auto;
+    border-collapse: collapse;
+    margin: 1rem auto;
 
     .titleRow {
       display: table-row;
@@ -127,11 +142,23 @@ export default {
         display: table-cell;
       }
       div.selector {
-        border: 1px solid blue;
-        border-radius: 3px;
+        border-bottom: 1px solid #666666;
+        & + .selector {
+           border-left: 1px solid #666666;
+        }
+        padding: .1rem .3rem;
+        writing-mode: sideways-lr;
+        width: 2rem;
+        height: 1rem;
+        text-align: left;
+        vertical-align: bottom;
+        padding: .2rem 0;
+        line-height: .8rem;
+        cursor: pointer;
+        background-color: #f5f5f5;
       }
       div.selected {
-        background-color: rgba(255, 0, 0, 0.26);
+        background-color: #e5e5e5;
       }
     }
     .selectorRow {
@@ -142,11 +169,40 @@ export default {
       }
       div.selector {
         font-weight: bold;
-        border: 1px solid blue;
-        border-radius: 3px;
+        margin: .1rem;
+        padding: .1rem .3rem;
+        text-align: right;
+        cursor: pointer;
+        // border: 1px solid blue;
+
+        &.facs {
+           background-color: #6CA5B466;
+           &.selected {
+             background-color: #6CA5B4;
+          }
+        }
+        &.trns {
+           background-color: #A7C4E566;
+           &.selected {
+             background-color: #A7C4E5;
+          }
+        }
+        &.text {
+           background-color: #AFEC7766;
+           &.selected {
+             background-color: #AFEC77;
+          }
+        }
+        &.anno {
+           background-color: #ECB57766;
+           &.selected {
+             background-color: #ECB577;
+          }
+        }
       }
+
       div.selected {
-        background-color: rgba(255, 0, 0, 0.26);
+        background-color: #dbfabf;
       }
     }
   }
