3f43e5f3069bafddc81c324c8c1b70c3fb9c1632
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2022-01-17 09:25:51 +0100

    ContextModal.vue

---
+++ src/components/ContextModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :id="divid">
+    <div class="contextModalHeader">
+    </div>
+    <div class="contextModalContent">
+    </div>
+    <div class="contextModalControl">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContextModal',
+  props: {
+    buttons: {
+      type: Array,
+      default: () => ([])
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
