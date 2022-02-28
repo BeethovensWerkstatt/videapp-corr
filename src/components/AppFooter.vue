4260ca10fae2872f55adcb164895e8907b4fd4d8
Johannes Kepper <kepper@edirom.de>
2022-02-28 22:28:57 +0100

    little adjustments to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,13 +14,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/variables.scss";
+
 .app-footer {
+  text-align: right;
+  padding: 0 1rem;
   position: fixed;
   bottom: 0px;
   width: 100%;
   height: 15pt;
-  font-size: 12pt;
-  color: gray;
-  background-color: black;
+  font-size: .6rem;
+  font-weight: 100;
+  color: darken($background-flat, 20%);
+  box-shadow: 0 3px 5px rgba(0,0,0,0.5) inset;
+  background-color: #333333;
+  background-image: -webkit-linear-gradient(top, #333333, #151515);
+  background-image: linear-gradient(to bottom, #333333, #151515);
 }
 </style>
