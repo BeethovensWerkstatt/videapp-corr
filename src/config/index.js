fd76463459c6e53996981cedfcad037f4afe6997
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2022-03-10 13:04:07 +0100

    version.json to promise

--- src/config/index.js
+++ src/config/index.js
@@ -1,32 +1,33 @@
 import osdDefault from './osd.default'
 import store from '@/store'
+import n from '@/store/names'
 import { Url } from '@/toolbox/net'
+import axios from 'axios'
 
 // TODO import from (e.g) public/config.json ?
 
 const config = {
   api: {
     works: {
-      // url: 'https://api.beethovens-werkstatt.de/module3/works.json'
-      // url: 'http://localhost:8080/exist/apps/api/module3/works.json'
-      url () { return getAPIURL('/module3/works.json') }
+      async url () { return await getAPIURL('/module3/works.json') }
     }
   },
   osd: osdDefault,
   couchdb: {
     defaultName: 'axios-data'
-  }
+  },
+  version: new Promise((resolve, reject) => {
+    axios.get('./version.json').then(({ status, data }) => {
+      if (status === 200) {
+        store.commit(n.mutations.SET_VERSION, data)
+        resolve(data)
+      } else {
+        console.error('failed to load version.json!', status)
+        reject(status)
+      }
+    })
+  })
 }
-
-config.version = require('../../public/version.json')
-
-fetch('./version.json').then(async resp => {
-  if (resp.ok) {
-    const v = await resp.json()
-    config.version = v
-    store.commit('SET_VERSION', config.version)
-  }
-})
 
 const API_HOST = {
   main: 'api.beethovens-werkstatt.de',
@@ -34,13 +35,14 @@ const API_HOST = {
   'jpv/dev': 'dev-api.beethovens-werkstatt.de'
 }
 
-export const getAPIURL = function (path) {
+export const getAPIURL = async function (path) {
+  const version = await config.version
   const url = new Url('https://api.beethovens-werkstatt.de/')
-  if (API_HOST[config.version.branch]) {
-    url.host = API_HOST[config.version.branch]
+  if (API_HOST[version.branch]) {
+    url.host = API_HOST[version.branch]
   }
   url.path = path
-  console.log(config.version.branch, 'API: ' + url)
+  console.log(version.branch, 'API: ' + url)
   return url.toString()
 }
 
