6e9f48edeec44e898386138238fdc4afec064878
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2022-09-16 07:54:41 +0200

    rebuild Desktop2

--- src/views/Desktop2.vue
+++ src/views/Desktop2.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="desk">
     <desktop-component divid="canvas" :width="1650" :height="1000"></desktop-component>
+    <div id="sidebar">
+      <div class="workTitle">
+        <strong>{{ workTitle }}</strong>
+        <p>{{ workLongTitle }}</p>
+      </div>
+      <div>
+        <div id="navigatorBox">
+          <div id="navigator"></div>
+        </div>
+        <div id="btnBox">
+          <btn-group>
+            <btn id="zoomIn" icon="plus" size="sm"></btn>
+            <btn id="zoomOut" icon="minus" size="sm"></btn>
+            <btn id="desktopOverview" icon="hResize" size="sm"></btn>
+          </btn-group>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import DesktopComponent from '@/components/DesktopComponent'
+import n from '@/store/names'
 
 /**
- * Desktop View without complaints
+ * Desktop View
  *
  * @module views/Desktop
  * @vue-computed {object[]} sources - list of available sources
@@ -20,7 +39,16 @@ export default {
     DesktopComponent
   },
   computed: {
-    ...mapGetters(['viewer', 'scale'])
+    ...mapGetters(['viewer', 'scale']),
+    work () {
+      return this.$store.getters[n.getters.getWork](this.$route.params.id)
+    },
+    workTitle () {
+      return this.work?.label ? this.work?.label[0].title : this.work?.title[0].title
+    },
+    workLongTitle () {
+      return this.work?.title[0].title
+    }
   }
 }
 </script>
@@ -36,6 +64,24 @@ export default {
   flex-flow: row nowrap;
   align-items: stretch;
   position: relative;
+}
+
+#dialog {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 800px;
+  height: 600px;
+  z-index: -1;
+  background-color: white;
+  border-radius: 5px;
+
+  &:hover {
+    border: 1px black solid;
+  }
+  &.active {
+    z-index: 10;
+  }
 }
 
 #canvas {
@@ -58,6 +104,43 @@ export default {
     &.active {
       background-color: rgba($color: #ffffff, $alpha: .5);
     }
+  }
+
+}
+
+#sidebar {
+  height: 100%;
+  width: calc(15rem - .5px);
+  flex: 1 1 auto;
+  border-left: .5px solid $border-color;
+  background-color: #f5f5f5;
+  padding: 5px;
+
+  & > div {
+    width: calc(100% - 1px - .4rem);
+    margin: .5rem .2rem;
+    border: .5px solid $border-color;
+    border-radius: .3rem;
+    background-color: #ffffff;
+    // box-shadow: 0 .1rem .5rem #00000033 inset;
+    padding: 8px;
+  }
+
+  .workTitle {
+    p {
+      color: gray;
+      font-size: 80%;
+      margin-bottom: 0;
+    }
+  }
+
+  #navigator {
+    height: 5rem;
+    width: 100%;
+  }
+
+  #btnBox > div {
+    display: inline-block;
   }
 }
 </style>
