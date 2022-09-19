d8d11a23048ee2aa0b3bf8dc813ed1482f6c260f
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2022-09-19 12:50:18 +0200

    create initSource action

--- src/store/sources/actions.js
+++ src/store/sources/actions.js
@@ -43,11 +43,13 @@ const actions = {
         work.label = work.title
         work.title = data.title
 
+        /*
         const positioner = new Positioner({
           width: getters.deskDimensions.width,
           hgap: getters.sourceHorizontalGap + (2 * getters.sourceMarginWidth),
           vgap: getters.sourceVerticalGap + getters.sourceHeaderHeight
         })
+        */
 
         dispatch(act.loadMovements, { work: work['@id'], ...data })
 
@@ -55,7 +57,7 @@ const actions = {
         dispatch(act.loadComplaints, { work: work['@id'], ...data })
 
         // console.log(data.manifestations)
-
+        /*
         const initSource = async (murl, index = 0) => {
           const resp = await axios.get(murl)
           const m = resp.data
@@ -81,7 +83,7 @@ const actions = {
               }
 
               // console.log(iiif)
-              if (iiif.sequences && iiif.sequences.length > 0) {
+              if (iiif.sequences?.length > 0) {
                 const canvases = iiif.sequences[0].canvases
                 const structures = iiif.structures
                 // console.log(structures)
@@ -183,57 +185,11 @@ const actions = {
 
                 // calc position
                 positioner.addSource(source)
-                /*
-                if (position.px > position.hgap && (position.px + position.hgap + source.maxDimensions.width) > getters.deskDimensions.width) {
-                  // start new line
-                  position.px = 0
-                  position.py += position.ph + position.vgap
-                  position.ph = source.maxDimensions.height
-                  source.position.x = position.px + position.hgap + (source.maxDimensions.width / 2)
-                  source.position.y = position.py + position.vgap + (source.maxDimensions.height / 2)
-                } else {
-                  // next horizontal position
-                  source.position.x = position.px + position.hgap + (source.maxDimensions.width / 2)
-                  source.position.y = position.py + position.vgap + (source.maxDimensions.height / 2)
-                  position.px += source.maxDimensions.width + position.hgap
-                  position.ph = Math.max(position.ph, source.maxDimensions.height)
-                }
-                */
                 // console.log(source.position)
 
                 // hack !!!
                 const sid = (new Url(source.id)).path.elements.pop()
                 if (positions[sid]) source.position = positions[sid]
-                /*
-                switch (sid) {
-                  // Op. 73
-                  case 'US-NYj_31_B393cp_no.5_errata.json':
-                    source.position = { x: 838, y: 220 }
-                    break
-                  case 'D-BNba_C73-9.json':
-                    source.position = { x: 316, y: 390 }
-                    break
-                  case 'D-BNba_HCB_BBr_9.json':
-                    source.position = { x: 838, y: 620 }
-                    break
-                  case 'A-Wn_SH.Beethoven.323.json':
-                    source.position = { x: 1370, y: 390 }
-                    break
-                  // Op. 120
-                  case 'D-BNba_Slg.H.C.Bodmer_HCB_C_Md_42.json':
-                    source.position = { x: 410, y: 185 }
-                    break
-                  case 'D-BNba_HCB_Mh_60.json':
-                    source.position = { x: 1235, y: 333 }
-                    break
-                  case 'D-BNba_NE_294.json':
-                    source.position = { x: 410, y: 590 }
-                    break
-                  case 'D-BNba_Slg.H.C.Bodmer_HCB_Mh_55.json':
-                    source.position = { x: 1235, y: 825 }
-                    break
-                }
-                */
 
                 // TODO unfold
                 source.structures = structures
@@ -245,13 +201,178 @@ const actions = {
             })
           }
         }
-
+        */
+        const initSource = (murl, index) => dispatch('initSource', { murl, index, workId })
         data.manifestations.forEach(initSource)
         finishProc()
         work.sourcesLoadFinished = true
       }
     }
   },
+
+  /**
+   * init source
+   * @memberof store.sources.actions
+   * @param {Function} commit
+   * @param {Object} state
+   * @param {String} murl url of source
+   * @param {Number} [index=0] number in list of source
+   * @param {Positioner} [positioner=new Positioner(...)] helper to layout sources on the desktop
+   * @param {String} [workId=""] id of work
+   */
+  async initSource ({ commit, state, getters }, {
+    murl,
+    index = 0,
+    positioner = new Positioner({
+      width: getters.deskDimensions.width,
+      hgap: getters.sourceHorizontalGap + (2 * getters.sourceMarginWidth),
+      vgap: getters.sourceVerticalGap + getters.sourceHeaderHeight
+    }),
+    workId = ''
+  }) {
+    const resp = await axios.get(murl)
+    const m = resp.data
+    // console.log(m)
+
+    const existingSource = state.sources.find(source => source.id === m.id)
+    if (existingSource === undefined) {
+      // get manifestation json
+      axios.get(m.iiif.manifest).then(res => {
+        const iiif = res.data
+
+        const source = {
+          id: m['@id'],
+          workId,
+          label: m.label,
+          // these values are updated later
+          maxDimensions: { width: 0, height: 0 },
+          position: { x: (150 + index * 400), y: 400 },
+          pages: [],
+          pageref: {},
+          rotation: 0,
+          singleLeaf: false
+        }
+
+        // console.log(iiif)
+        if (iiif.sequences?.length > 0) {
+          const canvases = iiif.sequences[0].canvases
+          const structures = iiif.structures
+          // console.log(structures)
+
+          // TODO workaround!
+          const exuuid = (atid) => {
+            const path = new Url(atid).path
+            return path.elements.pop()
+          }
+          // end TODO
+
+          // helper to prepare page object
+          const ctop = (canvas, place) => {
+            if (!canvas) {
+              return null
+            }
+            // console.log(canvas, place)
+            // default page height is 300mm if physicalScale is not defined
+
+            const physScale = (canvas.images[0] && canvas.images[0].resource && canvas.images[0].resource.service && canvas.images[0].resource.service.service && canvas.images[0].resource.service.service.physicalScale)
+              ? canvas.images[0].resource.service.service.physicalScale
+              : (300 / canvas.height)
+
+            const page = {
+              work: workId,
+              source: source.id,
+              id: canvas['@id'],
+              uuid: exuuid(canvas['@id']),
+              label: canvas.label,
+              place,
+              dimensions: { width: canvas.width * physScale, height: canvas.height * physScale },
+              pixels: { width: canvas.width, height: canvas.height },
+              uri: canvas.images[0].resource.service['@id'],
+              measures: [] // load measures?
+            }
+            commit(mut.LOAD_PAGE, page)
+
+            // get measure zones uri
+            const otherContent = canvas.otherContent
+            // console.log(otherContent)
+            if (otherContent) {
+              const mpos = otherContent.find(oc => oc.within?.label === TAG_MEASURE_POSITIONS)
+              if (mpos) {
+                // console.log(mpos)
+                // zones are loaded when page is opened first time
+                page.measures_uri = mpos['@id']
+              }
+              const psvg = otherContent.find(oc => oc.within?.label === TAG_SVG_SHAPES)
+              if (psvg) {
+                page.svg_shapes = psvg['@id']
+              }
+            }
+
+            return page
+          }
+
+          // iterate over pagepairs. First page is assumed recto.
+          for (var ci = 0; ci <= canvases.length; ci += 2) {
+            const recto = ci < canvases.length ? ctop(canvases[ci], 'recto') : null
+            const verso = ci > 0 ? ctop(canvases[ci - 1], 'verso') : null
+            // corresponding index in pagepair list
+            const pagenumber = source.pages.length
+
+            // TODO database/webSQL for fast retrievement of source+page for measure and/or complaint
+            if (recto) {
+              source.pageref[recto.uuid] = {
+                work: workId,
+                source: source.id,
+                n: pagenumber,
+                page: recto
+              }
+              recto.pagenumber = pagenumber
+              recto.place = 'recto'
+            }
+            if (verso) {
+              source.pageref[verso.uuid] = {
+                work: workId,
+                source: source.id,
+                n: pagenumber,
+                page: verso
+              }
+              verso.pagenumber = pagenumber
+              verso.place = 'verso'
+            }
+            // END TODO
+
+            const pagepair = { r: recto, v: verso }
+            source.pages.push(pagepair)
+            source.maxDimensions.width =
+              Math.max(source.maxDimensions.width,
+                ((pagepair.r ? pagepair.r.dimensions.width : 0) +
+                 (pagepair.v ? pagepair.v.dimensions.width : 0)))
+            source.maxDimensions.height =
+              Math.max(source.maxDimensions.height,
+                (pagepair.r ? pagepair.r.dimensions.height : 0),
+                (pagepair.v ? pagepair.v.dimensions.height : 0))
+          }
+          // console.log(source)
+
+          // calc position
+          positioner.addSource(source)
+          // console.log(source.position)
+
+          // hack !!!
+          const sid = (new Url(source.id)).path.elements.pop()
+          if (positions[sid]) source.position = positions[sid]
+
+          // TODO unfold
+          source.structures = structures
+
+          commit(mut.LOAD_SOURCE, source)
+        } else {
+          console.warn('no sequence for "' + m.label + '"', iiif)
+        }
+      })
+    }
+  },
+
   /**
    * load movements
    * @memberof store.sources.actions
